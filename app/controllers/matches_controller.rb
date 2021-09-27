41ec28f545b6b7c3e99c61452d97cf16b04adf3a
Ignacio Rubio <ignaciorubio3@gmail.com>
2021-09-27 22:37:33 +0200

    Added render_async for polling match data every minute

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,5 +1,9 @@
 class MatchesController < ApplicationController
   def index
+  end
+
+  def match_stats
     @matches = Match.all
+    render partial: "match_stats"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "matches#index"
+  get :match_stats, controller: :matches
+
+  resources :matches
 end
